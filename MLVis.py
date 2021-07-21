ca258d9b26ac8e1c11959a797f7629ceb8fdf493
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-21 11:57:35 -0400

    Compile, train, and evaluate model

--- MLVis.py
+++ MLVis.py
@@ -93,6 +93,41 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(10, **layer_settings)
 ])
 model.summary()
+
+
+# In[9]:
+
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+
+# In[10]:
+
+
+model.fit(train_images, train_labels, epochs=10)
+
+
+# In[8]:
+
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+
+# In[9]:
+
+
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+predictions = probability_model.predict(test_images)
+
+
+# In[11]:
+
+
 settings = {
   "edges": {
     "color": {
