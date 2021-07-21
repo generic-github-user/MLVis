14f3d90fb0525249420e5b78b7485c58262722ca
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-20 23:50:10 -0400

    Add project overview

---
+++ MLVis.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# MLVis provides tools for interactively visualizing machine learning models. It is designed to support multiple levels of abstraction, but focuses on the TensorFlow/Keras layers API. The main functionality is a web-based graph visualization of an ML model's components, which integrates various plots and statistics about each layer with intuitive representations of the model's overall structure.
