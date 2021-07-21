2433180637ab1588e061bcfd7dffb0d9596f35b3
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-21 11:54:45 -0400

    Add dataset loading code

--- MLVis.py
+++ MLVis.py
@@ -23,6 +23,47 @@ import webbrowser
 def factors(n):    
     return set(reduce(list.__add__, 
                 ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+
+# In[3]:
+
+
+factors(327)
+
+
+# In[4]:
+
+
+# todo: allow model editing (?)
+
+
+# In[ ]:
+
+
+# Sample model is based on code from TensorFlow/Keras image classification tutorial at https://www.tensorflow.org/tutorials/keras/classification
+
+
+# In[5]:
+
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+
+# In[3]:
+
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+# In[4]:
+
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
 settings = {
   "edges": {
     "color": {
